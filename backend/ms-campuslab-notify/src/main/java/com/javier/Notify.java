aec5995f3f4bd313901203ed7f2f04de68fe7900
javier <ja.quirogab@duocuc.cl>
2026-09-04 21:15:04 -0400

    commit

---
+++ backend/ms-campuslab-notify/src/main/java/com/javier/Notify.java
@@ -0,0 +1,12 @@
+package com.javier;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import org.springframework.boot.SpringApplication;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Notify {
+    public static void main(String[] args) {
+        SpringApplication.run(Notify.class, args);
+    }
+}
